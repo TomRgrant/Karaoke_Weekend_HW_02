0bb5dbee0368c4b01abe5047c07d0e907f9112be
Tom Grant <grntthomasronnie@gmail.com>
2023-02-03 21:48:00 +0000

    Added guest name + has name test

--- src/guest.py
+++ src/guest.py
@@ -1,3 +1,3 @@
 class Guest:
-    def __init__(self):
-        pass
+    def __init__(self, first_name, last_name, wallet, fav_song):
+        self.first_name = first_name
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,6 +1,12 @@
-# import unittest
-# from src.guest import Guest
+import unittest
+from src.guest import Guest
+from src.song import Song
 
-# class TestGuest(unittest.TestCase):
-#     def setUp(self):
-#         # self.book = Book("The Lord of the Rings", "J R R Tolkien", "Fantasy")
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest = Guest("James", "Corden", 50.00, Song("Let It Be", " Beatles"))
+
+    def test_has_name(self):
+        expected = "James"
+        actual = self.guest.first_name
+        self.assertEqual(expected, actual)
