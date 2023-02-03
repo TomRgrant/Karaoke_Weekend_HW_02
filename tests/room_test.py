d240d234bfefaf9d10823b5665ed90f4e4dd1bc8
Tom Grant <grntthomasronnie@gmail.com>
2023-02-03 22:10:35 +0000

    Added total room population + test has total population

--- tests/room_test.py
+++ tests/room_test.py
@@ -9,3 +9,8 @@ class TestRoom(unittest.TestCase):
         expected = 5
         actual = self.room.room_num
         self.assertEqual(expected, actual)
+
+    def test_has_total_population(self):
+        expected = 10
+        actual = self.room.total_pop
+        self.assertEqual(expected, actual)
