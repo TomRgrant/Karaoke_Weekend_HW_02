aae318a75a9f177b25c2e0b751cb6e59bba0f47f
Tom Grant <grntthomasronnie@gmail.com>
2023-02-03 20:51:19 +0000

    Inital commit

---
+++ tests/song_test.py
@@ -0,0 +1,6 @@
+import unittest
+from src.song import Song
+
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        # self.book = Book("The Lord of the Rings", "J R R Tolkien", "Fantasy")
