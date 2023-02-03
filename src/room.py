79abfabea961e1f17dba641df007c6ccf25e4e6f
Tom Grant <grntthomasronnie@gmail.com>
2023-02-03 23:01:30 +0000

    Added increase room pop function + test increases room population

--- src/room.py
+++ src/room.py
@@ -3,3 +3,8 @@ class Room:
         self.room_num = room_num
         self.total_pop = total_pop
         self.entry_fee = entry_fee
+        self.room_pop = []
+
+    def increase_room_pop(self, guest):
+        if len(self.room_pop) < self.total_pop:
+            self.room_pop.append(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,5 +1,6 @@
 import unittest
 from src.room import Room
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
@@ -18,4 +19,11 @@ class TestRoom(unittest.TestCase):
     def test_has_entry_fee(self):
         expected = 3.00
         actual = self.room.entry_fee
+        self.assertEqual(expected, actual)
+
+    def test_add_guest_increase_room_population(self):
+        self.guest = Guest("Frank", "Joel", 20.00, "Blame It On Me", "George Ezra")
+        self.room.increase_room_pop(self.guest.first_name)
+        expected = 1
+        actual = len(self.room.room_pop)
         self.assertEqual(expected, actual)
