30f6ecc432d088bb60598e9c32b86ac44bf0888e
Tom Grant <grntthomasronnie@gmail.com>
2023-02-03 22:18:52 +0000

    Added entry fee + test has entry fee

--- src/room.py
+++ src/room.py
@@ -2,3 +2,4 @@ class Room:
     def __init__(self, room_num, total_pop, entry_fee):
         self.room_num = room_num
         self.total_pop = total_pop
+        self.entry_fee = entry_fee
--- tests/room_test.py
+++ tests/room_test.py
@@ -3,7 +3,7 @@ from src.room import Room
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.room = Room(5, 10, 3)
+        self.room = Room(5, 10, 3.00)
 
     def test_has_room_number(self):
         expected = 5
@@ -13,4 +13,9 @@ class TestRoom(unittest.TestCase):
     def test_has_total_population(self):
         expected = 10
         actual = self.room.total_pop
+        self.assertEqual(expected, actual)
+
+    def test_has_entry_fee(self):
+        expected = 3.00
+        actual = self.room.entry_fee
         self.assertEqual(expected, actual)
